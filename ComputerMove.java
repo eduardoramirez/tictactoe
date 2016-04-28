71f6b6b4c1b095430d97fcdb9a9da92f605875ed
Eduardo Ramirez <edyr96@hotmail.com>
2016-04-27 20:02:36 -0700

    cleaned up a bit

--- ComputerMove.java
+++ ComputerMove.java
@@ -1,20 +1,19 @@
 /** interface ComputerPlay
   *
-  * This interface is meant to be used to 
-  * define an algorithm for a computer player 
-  * that is playing tic tac toe.  
+  * This interface is meant to be used to
+  * define an algorithm for a computer player
+  * that is playing tic tac toe.
   **/
-interface ComputerMove
-{
+interface ComputerMove {
   /**
     * This function chooses a move for the computer.
-    * It must choose a move that 1. is between 0-8 
-    * and 2. has not yet been taken by the player or 
+    * It must choose a move that 1. is between 0-8
+    * and 2. has not yet been taken by the player or
     * the computer. The function has
     * knowledge of the current state of the board.
     *
     * @Param board - Current state of the board
-    * @return computer's move 
+    * @return computer's move
     **/
   int makeAMove(char[] board);
 }
--- SequentialPlay.java
+++ SequentialPlay.java
@@ -1,13 +1,11 @@
-public class SequentialPlay implements ComputerMove
-{
+public class SequentialPlay implements ComputerMove {
   SequentialPlay() { }
-  public int makeAMove(char[] board)
-  {
+
+  public int makeAMove(char[] board) {
     int move;
     boolean errFlag = false;
 
-    for(move = 0; move < 9 && !errFlag; move++)
-    {
+    for (move = 0; move < 9 && !errFlag; move++) {
       // check spot is empty
       errFlag = board[move] == ' ' ? true : false;
     }
